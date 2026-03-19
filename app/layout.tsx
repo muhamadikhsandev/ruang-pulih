f2d1694bc1607cff514dd6de5cb8f12034f73beb
muhamadikhsandev <muhamadikhsan.dev@gmail.com>
2026-03-20 02:04:18 +0700

    first commit: cleaning up dependencies and initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ruang Pulih 🌸',
+  description: 'Tempat aman untuk healing dan berdamai dengan masa lalu',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="id">
+      <body className={`${inter.className} overflow-hidden`}>
+        {children}
+      </body>
+    </html>
+  )
+}
